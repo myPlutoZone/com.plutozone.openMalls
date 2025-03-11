0d48ff697b6a3a7a882280621135170db666fbb4
Sungwan Myung <pluto@brightsoft.co.kr>
2025-03-11 16:56:18 +0900

    [INSERT]setenv.sh/bat

---
+++ resource/tomcat/bin/setenv.bat
@@ -0,0 +1,6 @@
+REM [2021-05-25][pluto#plutozone.com][CREATE: spring.profiles.active for web.xml and spring.xml]
+REM set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_51
+REM set JAVA_OPTS=%JAVA_OPTS% -Dspring.profiles.active=local
+
+REM [2022-05-30][pluto#plutozone.com][CREATE: common.mode for web.xml]
+set JAVA_OPTS=%JAVA_OPTS% -Dcommon.mode=local
---
+++ resource/tomcat/bin/setenv.sh
@@ -0,0 +1,6 @@
+# [2021-05-25][pluto#plutozone.com][CREATE: spring.profiles.active for web.xml and spring.xml]
+# export JAVA_HOME=/usr/local/java/jdk1.7.0_51
+# export JAVA_OPTS="${JAVA_OPTS} -Dspring.profiles.active=local"
+
+# [2022-05-30][pluto#plutozone.com][CREATE: common.mode for web.xml]
+export JAVA_OPTS="${JAVA_OPTS} -Dcommon.mode=local"
